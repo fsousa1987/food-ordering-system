4e1331f90e97ffc72fe40ed1a473b8a0dad15e9b
Francisco Sousa <franciscoeds1987@gmail.com>
2024-05-05 22:13:27 -0300

    Adding Order Entity implementation methods

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -6,6 +6,8 @@ import java.util.Objects;
 
 public record Money(BigDecimal amount) {
 
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
+
     public boolean isGreaterThanZero() {
         return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;
     }
