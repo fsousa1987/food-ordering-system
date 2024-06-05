6fce3967bcde9a6fff7ad93ffddeaa7f1fedbf70
Francisco Sousa <franciscoeds1987@gmail.com>
2024-06-05 09:06:29 -0300

    Implementing Container module

---
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb21', 'b2fdb918-6080-4f87-9276-edf2bfd5d2e3', 500.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb23', 'b2fdb918-6080-4f87-9276-edf2bfd5d2e3', 100.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'b2fdb918-6080-4f87-9276-edf2bfd5d2e3', 600.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', 'b2fdb918-6080-4f87-9276-edf2bfd5d2e3', 200.00, 'DEBIT');
+
+
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb22', 'd215b5f8-0249-4dc5-89a3-51fd148cfb43', 100.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb26', 'd215b5f8-0249-4dc5-89a3-51fd148cfb43', 100.00, 'CREDIT');
